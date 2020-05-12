6ee28551d59f19368d334c9d872b4104dd8690a7
Matt Finnigan <mattfinniganwebdev@hotmail.com>
2020-05-12 13:13:18 +1000

    install dependencies

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+@section('title','Home Page')
+@section('content')
+  <h2>hi</h2>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title') | FishosFriend</title>
+
+      <!-- Styles -->
+      <style>
+        html, body {
+          min-height: 100vh;
+          margin: 0;
+        }
+      </style>
+  </head>
+  <body>
+    <div id="app">
+      @yield('content')
+    </div>
+  </body>
+</html>
